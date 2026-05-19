b06869a20c188d5103ad0732b6e2ff81e350d3b2
fathannnur <carelius02@gmail.com>
2026-05-19 18:15:54 +0700

    Menambahkan class derivedClass

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -13,3 +13,9 @@ public:
         cout << "Halo saya Function dari base class ";
     }
 };
+class derivedClass : public baseClass {
+public:
+    void perkenalan() {
+        cout << "Halo saya Function dari derived Class";
+    }
+};
